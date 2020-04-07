fb124f7714f8016765d63d5b25463da4e8ff4111
alvaroespinola <alvaro.espinola@gmail.com>
2020-04-06 23:44:36 -0400

    Permite Agregar Productos en Main Catalog

--- public_html/includes/templates/default.catalog/views/listing_product_column.inc.php
+++ public_html/includes/templates/default.catalog/views/listing_product_column.inc.php
@@ -22,4 +22,29 @@
   <button class="preview btn btn-default btn-sm" data-toggle="lightbox" data-target="<?php echo htmlspecialchars($link) ?>" data-require-window-width="768" data-max-width="980">
     <?php echo functions::draw_fonticon('fa-search-plus'); ?>
   </button>
+
+
+		<div class="buy_now" style="margin: 0em 0;">
+        <?php echo functions::form_draw_form_begin('buy_now_form', 'post'); ?>
+        <?php echo functions::form_draw_hidden_field('product_id', $product_id); ?>
+ 
+
+        <div class="form-group" align="center" style="padding: 10px;">
+          <label><?php echo language::translate('title_quantity', 'Quantity'); ?></label>
+          <div style="display: flex">
+            <div class="input-group" style="flex: 0 1 150px;margin-left:auto;margin-right:auto;">
+              <?php echo (!empty($quantity_unit['decimals'])) ? functions::form_draw_decimal_field('quantity', isset($_POST['quantity']) ? true : 1, $quantity_unit['decimals'], 1, null) : (functions::form_draw_number_field('quantity', isset($_POST['quantity']) ? true : 1, 1)); ?>
+  
+			  <?php echo !empty(reference::product($product_id)->quantity_unit_id) ? '<div class="input-group-addon">'. reference::product($product_id)->quantity_unit['name'] .'</div>' : ''; ?>
+
+            </div>
+          </div>
+		  <div align="center" style="flex: 1 0 auto; padding-top: 1em;">
+              <?php echo '<button class="btn btn-success" name="add_cart_product" value="true" type="submit"'. (($quantity <= 0 && !$orderable) ? ' disabled="disabled"' : '') .'>'. language::translate('title_add_to_cart', 'Add To Cart') .'</button>'; ?>
+          </div>
+        </div>
+
+        <?php echo functions::form_draw_form_end(); ?>
+      
+		
 </article>
